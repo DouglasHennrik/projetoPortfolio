ab0610654271f28bfe56907568fd3b0a4f5a33bf
DouglasHennrik <douglashennrik@gmail.com>
2024-10-25 02:14:47 -0300

    feat: Adição do Header e BackGround animado.

---
+++ src/app/components/header.tsx
@@ -0,0 +1,33 @@
+export default function header() {
+    return (
+        <body>
+            <header>
+
+                <div className="container">
+                    <div className="flex">
+
+                        <a className="logo" href="#">
+                        <span className="logo">&lt; </span>
+                        <span className="logoMain">Douglas</span>
+                        <span className="logo"> /&gt;</span>
+                        </a>
+
+
+                        <nav>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Sobre mim</a></li>
+                                <li><a href="#">Projetos</a></li>
+                                <li><a href="#">Contato</a></li>
+                            </ul>
+                        </nav>
+
+   
+                    </div>
+                </div>
+            </header>
+        </body>
+    );
+  }
+
+  
